ea66c061b6c2db2b4ca11ed4f742a1c3d09afa4d
elinachar <elinachar@gmail.com>
2019-01-29 18:54:56 +0200

    Image copyright on touch screens trial 6

--- app/assets/javascripts/copyright.js
+++ app/assets/javascripts/copyright.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function() {
 
   $('img').bind('contextmenu', function(e){
-    if(e.button == 2) { // right click (2) or long tap (0) on touchscreen
+    if(e.button == 2) { // right click (2) or long tap (0) on touchscreen (for touch devices see main.scss ->   body:-webkit-touch-callout: none; )
       alert("Sorry. This photo is copyrighted. Contact Theofilos Venardos if you want to download it.");
       return false;
     }
