20942b17f6677a3f60d04e6f94a848b52864228f
elinachar <elinachar@gmail.com>
2019-01-31 18:03:09 +0200

    Links in albums#show

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,9 @@ class Ability
     user ||= User.new
     if user.present?
       can [:show, :update, :destroy], User, id: user.id
+      can :manage, WeddingAlbum
+      can :manage, BaptismAlbum
+      can :manage, InteriorAlbum
       if user.admin?
         can :manage, :all
       end
