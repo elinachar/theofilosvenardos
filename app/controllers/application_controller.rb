0b84d55630f63cafcb7911f17699fe9b17d539a6
elinachar <elinachar@gmail.com>
2019-02-08 20:06:24 +0200

    Grid instagram photos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,18 +6,44 @@ class ApplicationController < ActionController::Base
   end
 
   def get_instagram_photos
+    # Get first 20 instagram photos
     @instagram_photos = []
+    index_array = (0..19).to_a
     media = Instagram.client.user_recent_media
-    count = 1
+
+    # Extract 6 photos with #wedding tag
     total_photos = 6
     tag = "wedding"
-    media.each do |media|
-      if media.caption.text.include? tag
-        @instagram_photos.push(media.images.thumbnail.url)
-        count += 1
+    media.each_with_index do |m ,i|
+      if m.caption.text.include? tag
+        @instagram_photos.push(m.images.thumbnail.url)
+        index_array.delete(i)
       end
-      break if count > total_photos
+      break if @instagram_photos.count == total_photos
     end
+
+    # If #wedding photos are less than 6
+
+    ## Sandbox mode : limited 20 photos
+    # then use other photos start from the newest photo
+    i = 0
+    while @instagram_photos.count < total_photos
+      @instagram_photos.push(media[i].images.thumbnail.url)
+      i += 1
+    end
+
+    ## Live mode : load next 20 photos
+    # maxId = media.pagination.max_id
+    # media = Instagram.client.user_recent_media(max_id: maxId)
+    # media.each_with_index do |m ,i|
+    #   if m.caption.text.include? tag
+    #     @instagram_photos.push(m.images.thumbnail.url)
+    #   end
+    #   break if @instagram_photos.count == total_photos
+    # end
+
+
   end
+
 
 end
