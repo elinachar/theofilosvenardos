f17f6fe54d4f984fa6f37e991d144d14d93f3a8a
elinachar <elinachar@gmail.com>
2020-01-27 12:06:06 +0200

    Routes with hypens instead of underscores

--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,16 @@ Rails.application.routes.draw do
     put 'users' => 'devise/registrations#update', :as => 'user_registration'
   end
 
-  resources :wedding_albums
-  resources :baptism_albums
-  resources :interior_albums
+  resources "wedding-albums", :controller => :wedding_albums, :as => :wedding_albums
+  resources "baptism-albums", :controller => :baptism_albums, :as => :baptism_albums
+  resources "interior-albums", :controller => :interior_albums, :as => :interior_albums
 
   root 'basic_pages#index'
   get 'basic_pages/index'
   get 'about', to: 'basic_pages#about'
-  get 'frequently_asked_questions', to: 'basic_pages#faq'
+  get 'frequently-asked-questions', to: 'basic_pages#faq'
   get 'contact', to: 'basic_pages#contact'
-  post 'thank_you', to: 'basic_pages#thank_you'
-  get 'basic_pages/under_construction'
+  post 'thank-you', to: 'basic_pages#thank_you'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
